50865cad2f5111679b854c9182a7451255c3ead4
dmitryagoshkov <agodimon@mail.ru>
2022-06-27 21:20:40 +0500

    создал структуру добавил фрагменты адаптеры и viewModels

---
+++ app/src/main/java/com/example/movieapp/screens/favorite/FavoriteFragment.kt
@@ -0,0 +1,16 @@
+package com.example.movieapp.screens.favorite
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movieapp.R
+
+class FavoriteFragment : Fragment(R.layout.favorite_fragment) {
+    
+    private lateinit var viewModel: FavoriteViewModel
+
+
+}
