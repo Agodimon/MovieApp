492d6d65fa2973111b1872cfc0eadaad6d443196
dmitryagoshkov <agodimon@mail.ru>
2022-06-27 21:13:23 +0500

    создал структуру

---
+++ app/src/main/java/com/example/movieapp/screens/main/MainFragment.kt
@@ -0,0 +1,15 @@
+package com.example.movieapp.screens.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movieapp.R
+
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+
+
+}
