09aaac8dfa5bffa1973a36c4fdb639ae04430b56
dmitryagoshkov <agodimon@mail.ru>
2022-06-29 20:47:34 +0500

    создаю список актеров v2

--- app/src/main/java/com/example/movieapp/data/retrofit/RetrofitRepository.kt
+++ app/src/main/java/com/example/movieapp/data/retrofit/RetrofitRepository.kt
@@ -1,11 +1,17 @@
 package com.example.movieapp.data.retrofit
 
 import com.example.movieapp.data.retrofit.api.RetrofitInstance
+import com.example.movieapp.models.actorsModel.ActorsModels
 import com.example.movieapp.models.movieModel.MoviesModel
 import retrofit2.Response
 
-class RetrofitRepository :RetrofitRepositoryInterface{
-    override suspend fun getMovie():Response<MoviesModel>{
+class RetrofitRepository : RetrofitRepositoryInterface {
+    override suspend fun getMovie(): Response<MoviesModel> {
         return RetrofitInstance.api.getPopularMovie()
     }
+
+    override suspend fun getActorsMovie(id: Int): Response<ActorsModels> {
+        return RetrofitInstance.api.getActorsMovie(id)
+    }
+
 }
--- app/src/main/java/com/example/movieapp/data/retrofit/RetrofitRepositoryInterface.kt
+++ app/src/main/java/com/example/movieapp/data/retrofit/RetrofitRepositoryInterface.kt
@@ -1,8 +1,10 @@
 package com.example.movieapp.data.retrofit
 
+import com.example.movieapp.models.actorsModel.ActorsModels
 import com.example.movieapp.models.movieModel.MoviesModel
 import retrofit2.Response
 
 interface RetrofitRepositoryInterface {
     suspend fun getMovie(): Response<MoviesModel>
+    suspend fun getActorsMovie(id: Int): Response<ActorsModels>
 }
